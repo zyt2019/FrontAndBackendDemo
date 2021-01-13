1e65cc83729a8853d81559838fd6939346ce0d61
zyt <469167991@qq.com>
2021-01-13 15:51:03 +0800

    A

---
+++ Backend/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public string GetTime()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss");
+        }
+    }
+}
